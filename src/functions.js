c5d0a56f7c0d1d4df4fc7636e4f66e3305e97825
Dovani Lenares <127882308+Dovani@users.noreply.github.com>
2023-09-04 06:44:07 -0300

    upload

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,10 @@
+function mudarTamanho(){
+    if(min-width > 768){
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
 function clickMenu(){
     if(menu.style.display == 'block'){
         menu.style.display = 'none';
